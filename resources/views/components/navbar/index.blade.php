fd4dcec49c6999eb16286c585d6303214a02102d
Martin Ro <martin-ro@users.noreply.github.com>
2022-05-09 18:42:22 +0800

    Fixed wrong css class

--- resources/views/components/navbar/index.blade.php
+++ resources/views/components/navbar/index.blade.php
@@ -27,7 +27,7 @@
             <div class="-mr-2 flex sm:hidden">
                 {{-- Mobile menu button --}}
                 <button type="button"
-                        class="rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                        class="rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500"
                         aria-controls="{{ $mobileMenuId }}"
                         @click="open = !open"
                         aria-expanded="false"
